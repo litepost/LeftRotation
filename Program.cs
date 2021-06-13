3d500badc58bc5734767dc031d2501a577598a3e
litepost <jeffrey.leupold@gmail.com>
2021-06-12 17:47:31 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+* https://www.hackerrank.com/challenges/array-left-rotation/problem
+*/
+
+using System;
+using System.Collections.Generic;
+
+namespace LeftRotation {
+    class Program {
+        static void Main(string[] args) {
+
+        }
+
+        public static List<int> rotateLeft(int d, List<int> input) {
+            for (int i = 0; i < d % input.Capacity; i++) {
+                input = rotateOne(input);
+            }
+
+            return input;
+        }
+        public static List<int> rotateOne(List<int> input) {
+            int temp = input[0];
+            input.RemoveAt(0);
+            input.Add(temp);
+
+            return input;
+        }
+    }
+}
